ea7f92bd5994ea1724df218abb402212d746ec4b
Thanakrit Nawasaykaew <thanakrit.nsk@gmail.com>
2022-06-20 00:18:32 +0700

    update claim

--- utils/custom-claim.go
+++ utils/custom-claim.go
@@ -7,8 +7,9 @@ import (
 // CustomClaims custom jwt standard claims
 type CustomClaims struct {
 	jwt.StandardClaims
-	Email       string   `json:"email"`
-	Name        string   `json:"name"`
-	LastName    string   `json:"last_name"`
-	Permissions []string `json:"permissions"`
+	Email          string   `json:"email"`
+	Name           string   `json:"name"`
+	LastName       string   `json:"last_name"`
+	Permissions    []string `json:"permissions"`
+	SignatureImage string   `json:"signature_image"`
 }
